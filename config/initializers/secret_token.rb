fb5ed05114319074393c921aa10f93d6f7bc8608
Abby Fichtner <haxrchick@gmail.com>
2012-01-25 12:21:39 -0500

    scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RenderMeRails::Application.config.secret_token = 'aed8475ce12e2db52506036563313e275a22ea9deb13bcf44b7fad808416c7c109f6eaac0f8c8a6a76d881f70e453d9b43ad04fd221c7519642a58f6c5943cc1'
